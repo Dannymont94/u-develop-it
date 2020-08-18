58d0248138dab1946a4d2f5b4d9588ea069396e7
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-18 18:30:26 -0400

    added get all, get by id, and delete by id routes

--- server.js
+++ server.js
@@ -1,10 +1,75 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  return console.log('Connected to the election database.');
+});
+
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+  const params = [];
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+     return res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+})
+
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates
+                WHERE id = ?`;
+
+  const params = [req.params.id];
+
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
+});
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
 
 
 
@@ -13,6 +78,8 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`);
+db.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+  })
 });

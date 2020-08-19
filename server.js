11431f138d4790d8775063373fb9e4d5b3dcf4ea
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-19 19:34:27 -0400

    added get routes for voter table

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const db = require('./db/database');
-
 const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());

b4540aabfac614f9b4c55407acbb54908b29413a
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-19 12:40:56 -0400

    added get and delete routes for parties table

--- server.js
+++ server.js
@@ -84,10 +84,52 @@ app.post('/api/candidate', ({ body }, res) => {
       return res.status(400).json({ error: err.message});
     }
 
-    res.json({
+    return res.json({
       message: 'success',
       data: body,
       id: this.lastID
+    });
+  });
+});
+
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    return res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res.json({
+      message: 'successfully deleted',
+      changes: this.changes
     });
   });
 });
